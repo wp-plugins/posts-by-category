cc2bfd214e02cef7b2d136d5c14b5f9bcaf55bb8
Shellbot <Shellbot@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-08 10:14:52 +0000

    First release
    git-svn-id: https://plugins.svn.wordpress.org/posts-by-category/trunk@984171 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ posts-by-category.php
@@ -0,0 +1,179 @@
+<?php
+/*
+Plugin Name: Posts By Category
+Plugin URI: http://shellbotics.com/wordpress-plugins/posts-by-category/
+Description: Display posts from a particular category or tag in a variety of ways.
+Version: 1.0.0
+Author: Shellbot
+Author URI: http://shellbotics.com
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+class sb_posts_by_category {
+
+    function __construct() {  
+        add_shortcode( 'sb_category_posts', array( $this, 'posts_shortcode' ) );
+    }
+    
+    
+    /* Shortcode ------------------------------------------------------------ */
+    
+    function posts_shortcode( $atts ) {
+        
+        extract( shortcode_atts(array(
+            'title' => '',
+            'type' => 'cat',
+            'cat' => '1',
+            'tag' => '1',
+            'show' => '-1',
+            'group_by' => 'none',
+            'show_image' => 'no',
+            'show_author' => 'no',
+            'cols' => '1',
+        ), $atts ) );
+
+        //Build a proper query
+        $query = array (
+            'posts_per_page' => $show,
+        ); 
+        
+        if( $type == 'tag' ) {
+            $query['tag_id'] = $tag;
+        } else {
+            $query['cat'] = $cat;
+        }
+        //var_dump($query);
+        
+        //Run query
+        $posts = get_posts( $query );
+        
+        if( !empty( $title ) ) {
+            $output = '<h3>' . $title .'</h3>';
+        }
+        
+        //Group posts if necessary
+        switch( $group_by ) {
+            case 'year':
+                $years = $this->group_by_year( $posts ); 
+                foreach( $years as $year => $posts ) {
+                    $output .= '<h4>' . $year . '</h4>';
+                    $output .= $this->output_posts( $posts );
+                }
+                break;
+            case 'month':
+                $months = $this->group_by_month( $posts );  
+                foreach( $months as $month => $posts ) {
+                    $output .= '<h4>' . $month . '</h4>';
+                    $output .= $this->output_posts( $posts );
+                }
+                break;
+            case 'letter':
+                $letters = $this->group_by_letter( $posts ); 
+                foreach( $letters as $letter => $posts ) {
+                    $output .= '<h4>' . $letter . '</h4>';
+                    $output .= $this->output_posts( $posts );
+                }
+                break;
+            default:
+                $output .= $this->output_posts( $posts );
+        }
+       
+        return $output;
+        
+    }
+    
+    
+    /* Grouping stuff ------------------------------------------------------- */
+    
+    function group_by_year( $ungrouped ) {
+        
+        $grouped = array();
+        
+        foreach( $ungrouped as $post ) {
+            $datetime_bits = explode( ' ', $post->post_date );
+            $date_bits = explode( '-', $datetime_bits['0'] );
+            $year = $date_bits['0']; 
+
+                $grouped[$year][] = $post;
+        }
+        
+        return $grouped;
+        
+    }
+    
+    function group_by_month( $ungrouped ) {
+        
+        $grouped = array();
+        
+        foreach( $ungrouped as $post ) {
+            $datetime_bits = explode( ' ', $post->post_date );
+            $date_bits = explode( '-', $datetime_bits['0'] );
+            $year = $date_bits['0'];
+            $month = $date_bits['1']; 
+            
+            $group = date( 'F', mktime( 0, 0, 0, $month, 10 ) ) . ' ' . $year;
+
+                $grouped[$group][] = $post;
+                
+        }
+        
+        return $grouped;
+        
+    }
+    
+    function group_by_letter( $ungrouped ) {
+        
+        $grouped = array();
+        
+        foreach( $ungrouped as $post ) {
+            
+            $letter = substr( $post->post_title, 0, 1 );
+  
+            $grouped[$letter][] = $post;
+            
+        }
+        
+        ksort( $grouped );
+        
+        return $grouped;
+        
+    }
+    
+    /* Output stuff --------------------------------------------------------- */
+    
+    function output_posts( $posts ) {
+        
+        $post_list = '';
+        
+        $post_list .= '<ul>';
+        
+        foreach( $posts as $post ) {
+            $post_list .= '<li><a href="' . get_permalink( $post->ID ) . '">' . $post->post_title . '</a></li>';
+        }
+        
+        $post_list .= '</ul>';
+        
+        return $post_list;
+        
+    }
+    
+}
+
+$sbpbc = new sb_posts_by_category();
---
+++ readme.txt
@@ -0,0 +1,48 @@
+=== Posts by Category ===
+Contributors: Shellbot
+Tags: category, posts, shortcode, list, tag
+Requires at least: 2.9
+Tested up to: 4.0
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=T3XBQZNXBEHPC
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Display a list of posts from a specific category or tag.
+
+== Description ==
+
+Posts by Category lets you display a list of posts pulled from a particular category or tag, and optionally
+group them by year, month or first letter of the post title.
+
+Current features include:
+
+* Shortcode allows post list to be inserted anywhere
+* Limit how many posts should be displayed
+* Group posts by year, month or first letter
+
+**Usage**
+
+To display the list of posts, add the following shortcode to your post or page.
+
+Default settings:
+
+`[sb_category_posts]`
+
+Custom settings:
+
+`[sb_category_posts show="10" cat="3" group_by="year"]`
+
+For full list of parameters see [the plugin release page](http://shellbotics.com/wordpress-plugins/posts_by_category/ "Posts by Category")
+
+== Installation ==
+
+1. Upload the 'posts-by-category' folder to the '/wp-content/plugins/' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Add the shortcode to your post or page
+4. Enjoy!
+
+== Changelog ==
+
+= 1.0 =
+* First version
