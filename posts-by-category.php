1825a33d74a111a27e27a135a49d86fa92004a94
Shellbot <Shellbot@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-27 12:00:04 +0000

    Add support and donate links
    git-svn-id: https://plugins.svn.wordpress.org/posts-by-category/trunk@1207449 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- posts-by-category.php
+++ posts-by-category.php
@@ -28,15 +28,70 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 class sb_posts_by_category {
 
-    function __construct() {  
-        add_shortcode( 'sb_category_posts', array( $this, 'posts_shortcode' ) );
+    function __construct() {
+      add_action('admin_notices', array( $this, 'show_admin_notice' ) );
+      add_action('admin_init', array( $this, 'dismiss_admin_notice' ) );
+      add_filter( 'plugin_action_links', array( $this, 'add_extra_links' ), 10, 5 );
+      add_shortcode( 'sb_category_posts', array( $this, 'posts_shortcode' ) );
     }
-    
-    
+
+    /* Display a notice that can be dismissed */
+
+    function show_admin_notice() {
+
+      if ( current_user_can( 'install_plugins' ) ) {
+
+        global $current_user ;
+          $user_id = $current_user->ID;
+          /* Check that the user hasn't already clicked to ignore the message */
+      	if ( ! get_user_meta($user_id, 'pbc_ignore_admin_notice') ) {
+          echo '<div class="updated"><p>';
+          printf(__('This Posts by Category plugin is <a href="%1$s" target="_blank">supported through Patreon</a>. If you find it useful, please consider a small donation. Thanks! | <a href="%2$s">Hide Notice</a>'), 'http://patreon.com/shellbot', '?pbc_admin_notice_ignore=0');
+          echo "</p></div>";
+      	}
+
+      }
+
+    }
+
+    function dismiss_admin_notice() {
+
+    	global $current_user;
+      $user_id = $current_user->ID;
+
+      /* If user clicks to ignore the notice, add that to their user meta */
+      if ( isset($_GET['pbc_admin_notice_ignore']) && '0' == $_GET['pbc_admin_notice_ignore'] ) {
+        add_user_meta($user_id, 'pbc_ignore_admin_notice', 'true', true);
+    	}
+
+    }
+
+    /* Support & donate links ----------------------------------------------- */
+
+    function add_extra_links( $actions, $plugin_file ) {
+        static $plugin;
+
+        if (!isset($plugin))
+          	$plugin = plugin_basename(__FILE__);
+
+        if ($plugin == $plugin_file) {
+
+        		$donate_link = array('donate' => '<a href="http://patreon.com/shellbot" target="_blank">' . __('Donate', 'shellbotics') . '</a>');
+        		$support_link = array('support' => '<a href="http://wordpress.org/support/plugin/posts-by-category" target="_blank">' . __('Support', 'posts-by-category') . '</a>');
+
+            $actions = array_merge($donate_link, $actions);
+            $actions = array_merge($support_link, $actions);
+
+        }
+
+      	return $actions;
+    }
+
+
     /* Shortcode ------------------------------------------------------------ */
-    
+
     function posts_shortcode( $atts ) {
-        
+
         extract( shortcode_atts(array(
             'title' => '',
             'type' => 'cat',
@@ -52,40 +107,40 @@ class sb_posts_by_category {
         //Build a proper query
         $query = array (
             'posts_per_page' => $show,
-        ); 
-        
+        );
+
         if( $type == 'tag' ) {
             $query['tag_id'] = $tag;
         } else {
             $query['cat'] = $cat;
         }
         //var_dump($query);
-        
+
         //Run query
         $posts = get_posts( $query );
-        
+
         if( !empty( $title ) ) {
             $output = '<h3>' . $title .'</h3>';
         }
-        
+
         //Group posts if necessary
         switch( $group_by ) {
             case 'year':
-                $years = $this->group_by_year( $posts ); 
+                $years = $this->group_by_year( $posts );
                 foreach( $years as $year => $posts ) {
                     $output .= '<h4>' . $year . '</h4>';
                     $output .= $this->output_posts( $posts );
                 }
                 break;
             case 'month':
-                $months = $this->group_by_month( $posts );  
+                $months = $this->group_by_month( $posts );
                 foreach( $months as $month => $posts ) {
                     $output .= '<h4>' . $month . '</h4>';
                     $output .= $this->output_posts( $posts );
                 }
                 break;
             case 'letter':
-                $letters = $this->group_by_letter( $posts ); 
+                $letters = $this->group_by_letter( $posts );
                 foreach( $letters as $letter => $posts ) {
                     $output .= '<h4>' . $letter . '</h4>';
                     $output .= $this->output_posts( $posts );
@@ -94,86 +149,86 @@ class sb_posts_by_category {
             default:
                 $output .= $this->output_posts( $posts );
         }
-       
-        return $output;
-        
-    }
-    
-    
-    /* Grouping stuff ------------------------------------------------------- */
-    
-    function group_by_year( $ungrouped ) {
-        
-        $grouped = array();
-        
-        foreach( $ungrouped as $post ) {
-            $datetime_bits = explode( ' ', $post->post_date );
-            $date_bits = explode( '-', $datetime_bits['0'] );
-            $year = $date_bits['0']; 
 
-                $grouped[$year][] = $post;
-        }
-        
-        return $grouped;
-        
+        return $output;
+
     }
-    
-    function group_by_month( $ungrouped ) {
-        
+
+
+    /* Grouping stuff ------------------------------------------------------- */
+
+    function group_by_year( $ungrouped ) {
+
         $grouped = array();
-        
+
         foreach( $ungrouped as $post ) {
             $datetime_bits = explode( ' ', $post->post_date );
             $date_bits = explode( '-', $datetime_bits['0'] );
             $year = $date_bits['0'];
-            $month = $date_bits['1']; 
-            
+
+                $grouped[$year][] = $post;
+        }
+
+        return $grouped;
+
+    }
+
+    function group_by_month( $ungrouped ) {
+
+        $grouped = array();
+
+        foreach( $ungrouped as $post ) {
+            $datetime_bits = explode( ' ', $post->post_date );
+            $date_bits = explode( '-', $datetime_bits['0'] );
+            $year = $date_bits['0'];
+            $month = $date_bits['1'];
+
             $group = date( 'F', mktime( 0, 0, 0, $month, 10 ) ) . ' ' . $year;
 
                 $grouped[$group][] = $post;
-                
+
         }
-        
+
         return $grouped;
-        
+
     }
-    
+
     function group_by_letter( $ungrouped ) {
-        
+
         $grouped = array();
-        
+
         foreach( $ungrouped as $post ) {
-            
+
             $letter = substr( $post->post_title, 0, 1 );
-  
+
             $grouped[$letter][] = $post;
-            
+
         }
-        
+
         ksort( $grouped );
-        
+
         return $grouped;
-        
+
     }
-    
+
     /* Output stuff --------------------------------------------------------- */
-    
+
     function output_posts( $posts ) {
-        
+
         $post_list = '';
-        
+
         $post_list .= '<ul>';
-        
+
         foreach( $posts as $post ) {
             $post_list .= '<li><a href="' . get_permalink( $post->ID ) . '">' . $post->post_title . '</a></li>';
         }
-        
+
         $post_list .= '</ul>';
-        
+
         return $post_list;
-        
+
     }
-    
+
 }
 
 $sbpbc = new sb_posts_by_category();
